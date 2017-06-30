429ebae59fbece34378e695a2ccfe83f2035aec4
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-30 12:23:32 +0200

    Utilisation de symbole pour les clés du hash "player"

--- ruby_game.rb
+++ ruby_game.rb
@@ -2,12 +2,12 @@ require 'colorize'
 
 nb_rows = 4
 nb_columns = 5
-player = { "x" => 2, "y" => 3 }
+player = { x: 2, y: 3 }
 
 for y in 1..nb_rows
   for x in 1..nb_columns
     motif = "X"
-    motif = "P".green if (x == player["x"]) && (y == player["y"])
+    motif = "P".green if (x == player[:x]) && (y == player[:y])
     print " #{motif}"
   end
   puts
