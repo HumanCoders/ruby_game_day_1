9d17d3e98667be10a1f341e396439fa6bdb706e9
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-30 12:29:53 +0200

    Définition d'une méthode "draw" permettant d'afficher la carte

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,14 +1,15 @@
 require 'colorize'
 
-nb_rows = 4
-nb_columns = 5
-player = { x: 2, y: 3 }
-
-for y in 1..nb_rows
-  for x in 1..nb_columns
-    motif = "X"
-    motif = "P".green if (x == player[:x]) && (y == player[:y])
-    print " #{motif}"
+def draw(nb_rows, nb_columns, player)
+  for y in 1..nb_rows
+    for x in 1..nb_columns
+      motif = "X"
+      motif = "P".green if (x == player[:x]) && (y == player[:y])
+      print " #{motif}"
+    end
+    puts
   end
   puts
 end
+
+draw(4, 5, { x: 2, y: 3 })
