8b96e8772461037da594817a1fa5d66b1aca220a
Matthieu Segret <matthieu.segret@gmail.com>
2017-06-30 11:46:01 +0200

    Affichage du joueur sur la carte

--- ruby_game.rb
+++ ruby_game.rb
@@ -1,9 +1,15 @@
+require 'colorize'
+
 nb_rows = 4
 nb_columns = 5
+player_x = 2
+player_y = 3
 
 for y in 1..nb_rows
   for x in 1..nb_columns
-    print " ."
+    motif = "X"
+    motif = "P".green if (x == player_x) && (y == player_y)
+    print " #{motif}"
   end
   puts
 end
